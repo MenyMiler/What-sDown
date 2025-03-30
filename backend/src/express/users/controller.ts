283883a46ad1c5d628cc9dd6056c5902bde037ce
MenyMiler <miler.yesodot@gmail.com>
2025-03-30 12:34:32 +0300

    without unnecessary commit

--- backend/src/express/users/controller.ts
+++ backend/src/express/users/controller.ts
@@ -5,10 +5,6 @@ import { updateOneRequestSchemaByGenesisId } from './validations';
 import { typeUser } from './interface';
 
 export class UsersController {
-    // static async updateOneByGenesisId(req: TypedRequest<typeof updateOneRequestSchemaByGenesisId>, res: Response) {
-    //     res.json(await UsersManager.updateOneByGenesisId(req.params.genesisId, req.body, req.user?.genesisId!));
-    // }
-
     static async updateOneByGenesisId(req: TypedRequest<typeof updateOneRequestSchemaByGenesisId>, res: Response) {
         res.json(await UsersManager.updateOneByGenesisId(req.params.genesisId, { type: req.body.type as unknown as typeUser }, req.user?.genesisId!));
     }
